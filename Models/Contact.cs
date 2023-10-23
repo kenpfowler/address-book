ac5a7d6a581ea9097c61654b60ebfa1cb1f834c7
Ken Fowler <development@kennyf.ca>
2023-10-23 19:07:46 -0400

    feat: created contact model

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -33,6 +34,7 @@ public class Contact
 
     [DataType(DataType.EmailAddress)]
     public string Email { get; set; }
+
     public DateTime Created { get; set; }
 
     [NotMapped]
