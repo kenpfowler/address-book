44a06d615846482ea8544bf112091e106941f7c5
Ken Fowler <development@kennyf.ca>
2023-10-23 19:07:31 -0400

    feat: registered services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using address_book;
 using address_book.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,8 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("ApplicationDbContext")));
-
+builder.Services.AddScoped<ContactsService>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IImageService, BasicImageService>();
 
 var app = builder.Build();
 
