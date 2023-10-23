297ac23065c783c9571b5d61128f4eac1c9ccdc4
Ken Fowler <development@kennyf.ca>
2023-10-23 19:08:43 -0400

    feat: configured controller

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -4,33 +4,49 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using address_book.Data;
+using address_book.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace address_book.Controllers
+namespace address_book.Controllers;
+public class ContactsController : Controller
 {
-    [Route("[controller]")]
-    public class ContactsController : Controller
+    private readonly ContactsService _service;
+
+    private readonly ILogger<ContactsController> _logger;
+
+    public ContactsController(ILogger<ContactsController> logger, ContactsService service)
     {
-        private readonly ApplicationDbContext _context;
+        _logger = logger;
+        _service = service;
+    }
 
-        private readonly ILogger<ContactsController> _logger;
+    [HttpGet]
+    async public Task<IActionResult> Index()
+    {
+        List<Contact> contacts = await _service.GetAllContacts();
+        return View(contacts);
+    }
 
-        public ContactsController(ILogger<ContactsController> logger, ApplicationDbContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
+    [HttpGet]
+    public IActionResult CreateContact()
+    {
+        Contact contact = new();
+        return View(contact);
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    async public Task<IActionResult> Create(Contact contact)
+    {
+        await _service.CreateContact(contact);
+        Contact newContact = new();
+        return View("CreateContact", newContact);
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
     }
 }
