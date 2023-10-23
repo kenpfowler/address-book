34485348f95f523308a5a06a5d66492bfcfc803a
Ken Fowler <development@kennyf.ca>
2023-10-23 19:06:47 -0400

    feat: added contact service and interface

---
+++ Services/ContactsService.cs
@@ -0,0 +1,51 @@
+﻿using address_book.Data;
+using address_book.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace address_book;
+
+public class ContactsService : IContactsService
+{
+
+    private readonly ApplicationDbContext _context;
+    private readonly IImageService _imageService;
+
+    public ContactsService(ApplicationDbContext context, IImageService imageService)
+    {
+        _context = context;
+        _imageService = imageService;
+    }
+
+    async public Task CreateContact(Contact contact)
+    {
+        byte[] bytes = await _imageService.ConvertFileToByteArrayAsync(contact.ImageFile);
+        contact.ImageData = bytes;
+        contact.ImageType = contact.ImageFile.ContentType;
+        await _context.Contacts.AddAsync(contact);
+        await _context.SaveChangesAsync();
+    }
+
+    public Task<Contact> DeleteContact(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Contact> EditContact(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Contact>> GetAllContacts()
+    {
+        List<Contact> contacts = await _context.Contacts.ToListAsync();
+        return contacts;
+    }
+
+    public Task<Contact> GetOneContact(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+}
---
+++ Services/IContactsService.cs
@@ -0,0 +1,12 @@
+﻿using address_book.Models;
+
+namespace address_book;
+
+public interface IContactsService
+{
+    Task<List<Contact>> GetAllContacts();
+    Task<Contact> GetOneContact(int id);
+    Task<Contact> EditContact(int id);
+    Task<Contact> DeleteContact(int id);
+    Task CreateContact(Contact contact);
+}
